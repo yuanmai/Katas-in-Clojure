75cf9b21b961301a88840eee2c68db6ef8c6b0ad
Yuan Mai <4registration@gmail.com>
2012-03-03 13:57:24 +0800

    test frame advance

--- src/kata/bowling.clj
+++ src/kata/bowling.clj
@@ -1,10 +1,12 @@
 (ns kata.bowling)
 
-(defn frames [rolls]
-  (cons (take
-         (condp #(= 10 (apply + (take %1 %2))) rolls
-           1 3
-           2 3
-           2)
-         rolls)
-        []))
+(defn frames
+  ([rolls] (frames rolls frames))
+  ([rolls f]
+     (cons (take
+            (condp #(= 10 (apply + (take %1 %2))) rolls
+              1 3
+              2 3
+              2)
+            rolls)
+           [])))
--- test/kata/test/bowling.clj
+++ test/kata/test/bowling.clj
@@ -5,5 +5,11 @@
 (deftest test-frame-and-bonuses
   (are [description frame-and-bonuses rolls] (= frame-and-bonuses (first (frames rolls)))
        "strike"     '(10 x y) '(10 x y z)
-       "spare"      '( 5 5 x) '( 5 5 x)
+       "spare"      '( 5 5 x) '( 5 5 x y)
        "open frame" '( 5 4)   '( 5 4 x)))
+
+(deftest test-frame-advance
+  (are [desciption advance rolls] (= advance (second (frames rolls list)))
+       "strike"     '(x y) '(10 x y z)
+       "spare"      '(x)   '( 5 5 x y)
+       "open frame" '(x)   '( 5 4 x)))
