e15df570d135a811a00e7da46dba639b32332630
Yuan Mai <4registration@gmail.com>
2012-03-03 13:53:13 +0800

    handle many frames

--- src/kata/bowling.clj
+++ src/kata/bowling.clj
@@ -1,9 +1,10 @@
 (ns kata.bowling)
 
 (defn frames [rolls]
-  (take
-   (condp #(= 10 (apply + (take %1 %2))) rolls
-     1 3
-     2 3
-     2)
-   rolls))
+  (cons (take
+         (condp #(= 10 (apply + (take %1 %2))) rolls
+           1 3
+           2 3
+           2)
+         rolls)
+        []))
--- test/kata/test/bowling.clj
+++ test/kata/test/bowling.clj
@@ -3,7 +3,7 @@
   (:use [clojure.test]))
 
 (deftest test-frame-and-bonuses
-  (are [description frame-and-bonuses rolls] (= frame-and-bonuses (frames rolls))
+  (are [description frame-and-bonuses rolls] (= frame-and-bonuses (first (frames rolls)))
        "strike"     '(10 x y) '(10 x y z)
        "spare"      '( 5 5 x) '( 5 5 x)
        "open frame" '( 5 4)   '( 5 4 x)))
