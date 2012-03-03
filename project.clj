9552efb2d8f66cbd6be00fba07d1ba8240738e61
Yuan Mai <4registration@gmail.com>
2012-03-03 12:24:39 +0800

    initial

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject kata "1.0.0-SNAPSHOT"
+  :description "Code Katas in Clojure"
+  :dependencies [[org.clojure/clojure "1.3.0"]])
---
+++ test/kata/test/bowling.clj
@@ -0,0 +1,6 @@
+(ns kata.test.bowling
+  (:use [kata.bowling])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
