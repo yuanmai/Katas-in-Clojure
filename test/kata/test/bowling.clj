6c7907cbad1e4ceef088995d73cf1c442cea20f3
Yuan Mai <4registration@gmail.com>
2012-03-03 14:26:16 +0800

    test frames for completed games

--- test/kata/test/bowling.clj
+++ test/kata/test/bowling.clj
@@ -13,3 +13,10 @@
        "strike"     '(x y z) '(10 x y z)
        "spare"      '(x y)   '( 5 5 x y)
        "open frame" '(x)     '( 5 4 x)))
+
+(deftest test-frames-for-competed-games
+  (are [description each-frame game] (= (repeat 10 each-frame)  (take 10 (frames game)))
+       "gutter game" [0 0] (repeat 0)
+       "perfect game" [10 10 10] (repeat 12 10)
+       "all spares" [5 5 5] (repeat 21 5)
+       "all ones" [1 1] (repeat 21 1)))
